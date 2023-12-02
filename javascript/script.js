dab40dcfea219c478316e95adad27387a1f18141
sachin <sachinkumar854881@gmail.com>
2023-12-02 09:11:10 +0530

    upload Website on Github

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+// scroll reveal
+ScrollReveal({ 
+    reset: true,
+    distance: '90px',
+    duration: 1500,
+    delay: 150
+    
+});
+ScrollReveal().reveal('.col1', { origin: 'top'});
+ScrollReveal().reveal('.pho', { origin: 'bottom'});
+ScrollReveal().reveal('.testimonial', { origin: 'top'});
+ScrollReveal().reveal('.tittle', { origin: 'left'}); 
